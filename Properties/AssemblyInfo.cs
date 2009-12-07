e3cb76fce7ae69527dceefb79447582e54bbc292
janwilmans <janwilmans@8027702c-e29e-11de-985c-a771f7a3a295>
2009-12-07 12:46:56 +0000

    converted project down to vs2005 optimized for aspect ratio

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DepCharter")]
+[assembly: AssemblyTitle("depcharter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Promexx")]
-[assembly: AssemblyProduct("DepCharter")]
-[assembly: AssemblyCopyright("Copyright © Promexx 2009")]
+[assembly: AssemblyCompany("Assembléon")]
+[assembly: AssemblyProduct("depcharter")]
+[assembly: AssemblyCopyright("Copyright © Assembléon 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("69fb453c-292d-47e4-bcb4-d31cc4872aaa")]
+[assembly: Guid("50e67920-e595-4461-a21b-962ed581c0c5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
