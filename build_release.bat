3ec79fe8ce76d2296b05ce947c9aca9bce40a8cf
janwilmans <janwilmans@gmail.com>
2017-04-29 21:19:22 +0200

    added batch file to create self-extractor

---
+++ build_release.bat
@@ -0,0 +1,4 @@
+del DepCharterRelease.exe > NUL
+rem not installing in the correct default directory, and not opening the install dir
+rem "C:\Program Files\WinRAR\Rar.exe" a -r -w"C:\Program Files\DepCharter" -sfx -z"winrar_sfx.config" DepCharterRelease.exe "C:\Program Files\DepCharter\*.*"
+pause
